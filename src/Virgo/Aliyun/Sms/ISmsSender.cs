48a410651f1decb96b4fbbef535122043e21e6d0
Jon <893703953@qq.com>
2019-01-21 21:58:09 +0800

    Submit Code

---
+++ src/Virgo/Aliyun/Sms/ISmsSender.cs
@@ -0,0 +1,18 @@
+﻿using Aliyun.Acs.Dysmsapi.Model.V20170525;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virgo.Aliyun.Sms
+{
+    public interface ISmsSender
+    {
+        /// <summary>
+        /// 短信发送服务
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        Task<SendSmsResponse> SendSmsAsync(SendSmsRequest request);
+    }
+}
