0e294778b929cdd49a59949a1730d49f8ee81bb9
Jon <893703953@qq.com>
2019-01-25 23:38:23 +0800

    Submit Code

---
+++ sample/Virgo.AspNetCore/Controllers/IpController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Virgo.IP;
+
+namespace Virgo.AspNetCore.Controllers
+{
+    public class IpController : Controller
+    {
+        [HttpGet]
+        public IActionResult Search(string ip="39.108.80.222")
+        {
+            var result = IpHelper.Search(ip);
+            return Json(result);
+        }
+    }
+}
