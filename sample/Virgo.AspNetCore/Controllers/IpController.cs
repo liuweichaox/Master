1b510802e8e47e08851199e1c77f5712a31f0931
Jon <893703953@qq.com>
2019-01-26 14:14:09 +0800

    Submit Code

--- sample/Virgo.AspNetCore/Controllers/IpController.cs
+++ sample/Virgo.AspNetCore/Controllers/IpController.cs
@@ -4,14 +4,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Virgo.IP;
+using Virgo.Extensions;
+using System.Net;
 
 namespace Virgo.AspNetCore.Controllers
 {
     public class IpController : Controller
     {
         [HttpGet]
-        public IActionResult Search(string ip="39.108.80.222")
-        {
+        public IActionResult Search(string ip)
+        {          
+           
+            if (ip.IsNullOrEmpty())
+            {
+                ip = HttpContext.Connection.RemoteIpAddress.ToString();
+            }
+            if (ip == "::1")
+            {
+                ip = "127.0.0.1";
+            }
+            if (!IPAddress.TryParse(ip, out var iPAddress))
+            {
+                ip = "127.0.0.1";
+            }
             var result = IpHelper.Search(ip);
             return Json(result);
         }
