472e9b5a42ed4cbfd0b1e1387a43c6424264e5de
刘伟超 <liuweic.@52mf.com>
2019-07-17 11:22:04 +0800

    Submit Code

---
+++ src/Virgo/Extensions/RandomExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Virgo.Extensions
+{
+    public static class RandomExtensions
+    {
+        /// <summary>
+        /// 生成真正的随机数
+        /// </summary>
+        /// <param name="r"></param>
+        /// <param name="seed"></param>
+        /// <returns></returns>
+        public static int StrictNext(this Random r, int seed = Int32.MaxValue)
+        {
+            return new Random((int)Stopwatch.GetTimestamp()).Next(seed);
+        }
+    }
+}
