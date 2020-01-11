b67115a08ec1ba4810281c6444da3d28c3be28b7
伟超 刘 <893703953@qq.com>
2020-01-11 14:53:33 +0800

    动态单元格转换

---
+++ src/Virgo/Extensions/ExcelExtensions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Virgo.Extensions
+{
+    /// <summary>
+    /// Excel拓展
+    /// </summary>
+    public static class ExcelExtensions
+    {
+        /// <summary>
+        /// 动态单元格转换
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static List<T> DynmaicCellToList<T>(this string[,] data) where T : class, new()
+        {
+            var cells = new List<T>();
+            var propertyPosition = new Dictionary<int, string>();
+            var type = typeof(T);
+            var propertyInfos = type.GetProperties();
+            for (int rowIndex = 0; rowIndex < data.GetLength(0); rowIndex++)
+            {
+                var rowInstance = Activator.CreateInstance<T>();
+                for (int columnIndex = 0; columnIndex < data.GetLength(1); columnIndex++)
+                {
+                    var cell = data[rowIndex, columnIndex];
+                    if (rowIndex == 0)
+                    {
+                        var propertyName = propertyInfos.SingleOrDefault(x => x.GetCustomAttribute<DescriptionAttribute>()?.Description == cell)?.Name;
+                        if (propertyName != null)
+                        {
+                            propertyPosition.Add(columnIndex, propertyName);
+                        }
+                    }
+                    else
+                    {
+                        var propertyName = propertyPosition.GetValueOrDefault(columnIndex);
+                        if (propertyName != null)
+                        {
+                            var property = type.GetProperty(propertyName);
+                            try
+                            {
+                                var value = Convert.ChangeType(cell, property.PropertyType);
+                                property.SetValue(rowInstance, value);
+                            }
+                            catch
+                            {
+                                //自定义异常：单元格数据转换失败
+                            }
+                        }
+                    }
+                }
+                if (rowIndex != 0)
+                {
+                    cells.Add(rowInstance);
+                }
+            }
+            return cells;
+        }
+    }
+}
---
+++ test/Virgo.Tests/Extensions/ExcelExtensions_Tests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xunit;
+using Virgo.Extensions;
+using Shouldly;
+using System.Linq;
+
+namespace Virgo.Tests.Extensions
+{
+    /// <summary>
+    /// <see cref="ExcelExtensions"/>测试
+    /// </summary>
+    public class ExcelExtensions_Tests
+    {
+        /// <summary>
+        /// 动态单元格转换
+        /// </summary>
+        [Fact]
+        public void DynmaicCellToList_Test()
+        {
+            var dynmaicCell = new string[3, 3]
+            {
+                {" 姓  名 "," 邮  箱 ","创建时间" },
+                {"Jon","123456@gmail.com",DateTime.Now.ToString() },
+                {"Allen","98765@gmail.com",DateTime.Now.ToString()}
+            };
+            var templates = dynmaicCell.DynmaicCellToList<ExcelTemplate>();
+            templates.Any(x => x.Name == "Jon").ShouldBe(true);
+        }
+    }
+
+    /// <summary>
+    /// Excel导入模板
+    /// </summary>
+    public class ExcelTemplate
+    {
+        /// <summary>
+        /// 姓名 
+        /// </summary>
+        [Description(" 姓  名 ")]
+        public string Name { get; set; }
+        /// <summary>
+        /// 邮箱 
+        /// </summary>
+        [Description(" 邮  箱 ")]
+        public string Email { get; set; }
+        /// <summary>
+        /// 创建时间 
+        /// </summary>
+        [Description("创建时间")]
+        public DateTime CreateTime { get; set; }
+    }
+}
