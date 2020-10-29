1fa0cbbb878193533cf6dd25f662703511c273b1
刘伟超 <893703953@qq.com>
2020-10-29 14:59:14 +0800

    redis 锁 完善

--- src/Virgo.Redis/RedisLock.cs
+++ src/Virgo.Redis/RedisLock.cs
@@ -21,31 +21,81 @@ namespace Virgo.Redis
         /// <summary>
         /// 获取锁
         /// </summary>
-        /// <param name="key">要操作的Key</param>
-        /// <param name="value">Guid</param>
+        /// <param name="key">锁的键</param>
+        /// <param name="value">当前占用值</param>
+        /// <param name="second">过期时间(秒)</param>
+        /// <returns></returns>
+        public bool LockTake(string key, string value, int second)
+        {
+            return _redis.LockTake(key, value, TimeSpan.FromSeconds(second));
+        }
+
+        /// <summary>
+        /// 释放锁
+        /// </summary>
+        /// <param name="key">锁的键</param>
+        /// <param name="value">当前占用值</param>
+        /// <returns></returns>
+        public bool LockRelease(string key, string value)
+        {
+            return _redis.LockRelease(key, value);
+        }
+
+        /// <summary>
+        /// 使用锁执行一个方法
+        /// </summary>
+        /// <param name="key">锁的键</param>
+        /// <param name="value">当前占用值</param>
+        /// <param name="second">过期时间(秒)</param>
+        /// <param name="executeAction">要执行的方法</param>
+        public void ExecuteWithLock(string key, string value, int second, Action executeAction)
+        {
+            if (executeAction == null) return;
+            if (LockTake(key, value, second))
+            {
+                try
+                {
+                    LockWatchDogStart(key, value, second);
+                    executeAction();
+                }
+                finally
+                {
+                    LockWatchDogStop();
+                    LockRelease(key, value);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 获取锁
+        /// </summary>
+        /// <param name="key">锁的键</param>
+        /// <param name="value">当前占用值</param>
         /// <param name="second">过期时间(秒)</param>
         /// <returns></returns>
         public async Task<bool> LockTakeAsync(string key, string value, int second)
         {
             return await _redis.LockTakeAsync(key, value, TimeSpan.FromSeconds(second));
         }
+
         /// <summary>
         /// 释放锁
         /// </summary>
-        /// <param name="key">要操作的Key</param>
-        /// <param name="value">Guid</param>
+        /// <param name="key">锁的键</param>
+        /// <param name="value">当前占用值</param>
         /// <returns></returns>
         public async Task<bool> LockReleaseAsync(string key, string value)
         {
             return await _redis.LockReleaseAsync(key, value);
         }
+
         /// <summary>
         /// Redis分布式锁续期问题解决方案
         /// 设置redis分布式锁情况下，客户端获取锁成功执行redis操作时，同时开启一个后台线程（看门狗），
         /// 每隔过期时间的1/3时检查是否还持有锁，如果持有则自动续期
         /// </summary>
-        /// <param name="key">要操作的Key</param>
-        /// <param name="value">Guid</param>
+        /// <param name="key">锁的键</param>
+        /// <param name="value">当前占用值</param>
         /// <param name="second">过期时间(秒)</param>
         public void LockWatchDogStart(string key, string value, int second)
         {
@@ -56,11 +106,12 @@ namespace Virgo.Redis
             };
             _timer.Start();
         }
+
         /// <summary>
         /// 续期锁
         /// </summary>
-        /// <param name="key">要操作的Key</param>
-        /// <param name="value">Guid</param>
+        /// <param name="key">锁的键</param>
+        /// <param name="value">当前占用值</param>
         /// <param name="second">过期时间(秒)</param>
         private void LockRenew(string key, string value, int second)
         {
@@ -68,9 +119,8 @@ namespace Virgo.Redis
             var current = _redis.StringGet(key, CommandFlags.PreferSlave);
             if (current == value)
             {
-                Console.WriteLine($"--设置前剩余过期时间为{_redis.KeyTimeToLive(key)}");
-                // 设置锁的过期时间
-                _redis.KeyExpire(key, DateTime.Now.AddSeconds(second));
+                Console.WriteLine($"--设置前剩余过期时间为{_redis.KeyTimeToLive(key)}");                
+                _redis.KeyExpire(key, DateTime.Now.AddSeconds(second));// 设置锁的过期时间
             }
             else
             {
@@ -78,13 +128,39 @@ namespace Virgo.Redis
                 Console.WriteLine($"--设置过期时间失败，当前value:{current},redisvalue:{value}");
             }
         }
+
         /// <summary>
-        /// 设置redis分布式锁情况下，客户端获取锁并执行结束redis操作时，释放后台线程（开门狗）
+        /// 设置redis分布式锁情况下，客户端获取锁并执行结束redis操作时，释放后台线程（看门狗）
         /// </summary>
         public void LockWatchDogStop()
         {
-            Console.WriteLine($"Stop——{System.Threading.Thread.CurrentThread.ManagedThreadId}关闭开门狗，时间为:{DateTime.Now}");
+            Console.WriteLine($"Stop——{System.Threading.Thread.CurrentThread.ManagedThreadId}关闭看门狗，时间为:{DateTime.Now}");
             _timer.Stop();
+        }
+
+        /// <summary>
+        /// 异步使用锁执行一个方法
+        /// </summary>
+        /// <param name="key">锁的键</param>
+        /// <param name="value">当前占用值</param>
+        /// <param name="second">过期时间(秒)</param>
+        /// <param name="executeAction">要执行的方法</param>
+        public async Task ExecuteWithLockAsync(string key, string value, int second, Func<Task> executeAction)
+        {
+            if (executeAction == null) return;
+            if (await LockTakeAsync(key, value, second))
+            {
+                try
+                {
+                    LockWatchDogStart(key, value, second);
+                    await executeAction();
+                }
+                finally
+                {
+                    LockWatchDogStop();
+                    await LockReleaseAsync(key, value);
+                }
+            }
         }
     }
 }
