9d640a4ee50e041725b3f4749e97beb532df44f8
刘伟超 <893703953@qq.com>
2020-11-10 17:23:47 +0800

    MQ单元测试

--- src/Virgo.RabbitMQ/RabbitMQProxy.cs
+++ src/Virgo.RabbitMQ/RabbitMQProxy.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Virgo.RabbitMQ
 {
@@ -19,11 +20,149 @@ namespace Virgo.RabbitMQ
         {
             _mQConfiguration = mQConfiguration;
         }
-
         /// <summary>
         /// 发布消息
         /// </summary>
         /// <param name="queueName">队列名称</param>
+        /// <param name="model">发送数据</param>
+        /// <returns>是否成功</returns>
+        public bool Publish(string queueName, object model)
+        {
+            try
+            {
+                // 1.实例化连接工厂。
+                var factory = new ConnectionFactory
+                {
+                    VirtualHost = "/",
+                    HostName = _mQConfiguration.HostName,
+                    Port = _mQConfiguration.Port,
+                    UserName = _mQConfiguration.UserName,
+                    Password = _mQConfiguration.Password
+                };
+                // 2.创建连接
+                using var conn = factory.CreateConnection();
+                // 3.创建频道
+                using var channel = conn.CreateModel();
+                // 4. 申明队列(指定durable:true,告知rabbitmq对消息进行持久化)
+                channel.QueueDeclare(queueName, true, false, false, null);
+                //将消息标记为持久性 - 将IBasicProperties.SetPersistent设置为true
+                var props = channel.CreateBasicProperties();
+                props.Persistent = true;
+                // 5. 构建byte消息数据包
+                var msg = JsonConvert.SerializeObject(model);
+                var messageBodyBytes = Encoding.UTF8.GetBytes(msg);
+                // 6. 发送数据包(指定basicProperties)
+                channel.BasicPublish("", queueName, props, messageBodyBytes);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// 消息订阅
+        /// </summary>
+        /// <typeparam name="T">消息模型</typeparam>
+        /// <param name="queueName">队列名称</param>
+        /// <param name="routingKey">路由键</param>
+        /// <param name="func">处理消息</param>
+        public void Subscribe<T>(string queueName, Func<T, bool> func) where T : class
+        {
+            // 1.实例化连接工厂。
+            var factory = new ConnectionFactory
+            {
+                VirtualHost = "/",
+                HostName = _mQConfiguration.HostName,
+                Port = _mQConfiguration.Port,
+                UserName = _mQConfiguration.UserName,
+                Password = _mQConfiguration.Password
+            };
+            // 2.建立连接
+            using var conn = factory.CreateConnection();
+            // 3.创建频道 
+            using var channel = conn.CreateModel();
+            // 4.申明队列(指定durable:true,告知rabbitmq对消息进行持久化)
+            channel.QueueDeclare(queueName, true, false, false, null);
+            //设置prefetchCount : 1来告知RabbitMQ，在未收到消费端的消息确认时，不再分发消息，也就确保了当消费端处于忙碌状态时，不再分配任务。
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+            // 5.构造消费者实例
+            var consumer = new EventingBasicConsumer(channel);
+            // 6.绑定消息接收后的事件委托
+            consumer.Received += (model, ea) =>
+            {
+                var msg = "";
+                var bytes = ea.Body;
+                msg = Encoding.UTF8.GetString(bytes);
+                var instance = JsonConvert.DeserializeObject<T>(msg);
+                //7. 模拟耗时
+                var success = func(instance);
+                //应答确认
+                if (success)
+                {
+                    // 8. 发送消息确认信号（手动消息确认）
+                    channel.BasicAck(ea.DeliveryTag, false);
+                }
+            };
+            // 9.启动消费者
+            //autoAck:true；自动进行消息确认，当消费端接收到消息后，就自动发送ack信号，不管消息是否正确处理完毕
+            //autoAck:false；关闭自动消息确认，通过调用BasicAck方法手动进行消息确认
+            channel.BasicConsume(queueName, false, consumer);
+        }
+        /// <summary>
+        /// 异步消息订阅
+        /// </summary>
+        /// <typeparam name="T">消息模型</typeparam>
+        /// <param name="queueName">队列名称</param>
+        /// <param name="routingKey">路由键</param>
+        /// <param name="func">处理消息</param>
+        public void SubscribeAsync<T>(string queueName, Func<T, Task<bool>> func) where T : class
+        {
+            // 1.实例化连接工厂。
+            var factory = new ConnectionFactory
+            {
+                VirtualHost = "/",
+                HostName = _mQConfiguration.HostName,
+                Port = _mQConfiguration.Port,
+                UserName = _mQConfiguration.UserName,
+                Password = _mQConfiguration.Password
+            };
+            // 2.建立连接
+            using var conn = factory.CreateConnection();
+            // 3.创建频道 
+            using var channel = conn.CreateModel();
+            // 4.申明队列(指定durable:true,告知rabbitmq对消息进行持久化)
+            channel.QueueDeclare(queueName, true, false, false, null);
+            //设置prefetchCount : 1来告知RabbitMQ，在未收到消费端的消息确认时，不再分发消息，也就确保了当消费端处于忙碌状态时，不再分配任务。
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+            // 5.构造消费者实例
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            // 6.绑定消息接收后的事件委托
+            consumer.Received += async (model, ea) =>
+            {
+                var msg = "";
+                var bytes = ea.Body;
+                msg = Encoding.UTF8.GetString(bytes);
+                var instance = JsonConvert.DeserializeObject<T>(msg);
+                // 7.模拟耗时
+                var success = await func(instance);
+                //应答确认
+                if (success)
+                {
+                    // 8.发送消息确认信号（手动消息确认）
+                    channel.BasicAck(ea.DeliveryTag, false);
+                }
+            };
+            // 9.启动消费者
+            //autoAck:true；自动进行消息确认，当消费端接收到消息后，就自动发送ack信号，不管消息是否正确处理完毕
+            //autoAck:false；关闭自动消息确认，通过调用BasicAck方法手动进行消息确认
+            channel.BasicConsume(queueName, false, consumer);
+        }
+
+        /// <summary>
+        /// 发布消息-Exchange模式
+        /// </summary>
+        /// <param name="queueName">队列名称（空则不定义队列）</param>
         /// <param name="model">发送数据</param>
         /// <param name="exchangeName">交换机名称</param>
         /// <param name="exchangeType">交换机类型</param>
@@ -31,45 +170,39 @@ namespace Virgo.RabbitMQ
         /// <returns>是否成功</returns>
         public bool Publish(string queueName, object model, string exchangeName, string exchangeType, string routingKey)
         {
-            if (string.IsNullOrEmpty(_mQConfiguration.HostName))
-            {
-                return false;
-            }
-            if (model == null)
-            {
-                return false;
-            }
+
             try
             {
-                // 实例化连接工厂。
+                // 1.实例化连接工厂。
                 var factory = new ConnectionFactory
                 {
                     VirtualHost = "/",
                     HostName = _mQConfiguration.HostName,
                     Port = _mQConfiguration.Port,
-                    UserName = _mQConfiguration.UserName ?? "",
-                    Password = _mQConfiguration.Password ?? ""
+                    UserName = _mQConfiguration.UserName,
+                    Password = _mQConfiguration.Password
                 };
-                // 创建连接、信道。
+                // 2.建立连接
                 using var conn = factory.CreateConnection();
+                // 3.创建频道 
                 using var channel = conn.CreateModel();
-                //定义交换机
+                // 4.定义交换机
                 channel.ExchangeDeclare(exchangeName, exchangeType, true, false, null);
-                // 声明队列，标记为持久性
-                channel.QueueDeclare(queueName, true, false, false, null);
-                //绑定交换机-队列
-                channel.QueueBind(queueName, exchangeName, routingKey, null);
-                //序列化对象
+                //交换器模式，队列不是必须的，也可以根据指定队列获取消息
+                if (string.IsNullOrEmpty(queueName) == false)
+                {
+                    // 定义队列
+                    channel.QueueDeclare(queueName, true, false, false, null);
+                    //绑定交换机-队列
+                    channel.QueueBind(queueName, exchangeName, routingKey, null);
+                }
+                // 5. 构建byte消息数据包
                 var msg = JsonConvert.SerializeObject(model);
-                //在MQ上定义一个持久化队列，如果名称相同不会重复创建
                 var messageBodyBytes = Encoding.UTF8.GetBytes(msg);
                 // 将消息标记为持久性。
                 var props = channel.CreateBasicProperties();
-                // MIME类型
-                props.ContentType = "text/plain";
-                //非持久化1,持久化2
-                props.DeliveryMode = 2;
-                //发送消息到队列
+                props.Persistent = true;
+                // 6. 发送数据包(指定basicProperties)
                 channel.BasicPublish(exchangeName, routingKey, props, messageBodyBytes);
                 return true;
             }
@@ -78,105 +211,115 @@ namespace Virgo.RabbitMQ
                 return false;
             }
         }
-
         /// <summary>
-        /// 消息订阅
+        /// 消息订阅-Exchange模式
         /// </summary>
         /// <typeparam name="T">消息模型</typeparam>
-        /// <param name="queueName">队列名称</param>
+        /// <param name="queueName">队列名称（空则为临时队列）</param>
         /// <param name="exchangeName">交换机名称</param>
         /// <param name="exchangeType">交换机类型</param>
         /// <param name="routingKey">路由键</param>
         /// <param name="func">处理消息</param>
         public void Subscribe<T>(string queueName, string exchangeName, string exchangeType, string routingKey, Func<T, bool> func) where T : class
         {
-            var isClose = false;//队列服务端是否关闭
-            do
+            // 1.实例化连接工厂。
+            var factory = new ConnectionFactory
             {
-                try
+                VirtualHost = "/",
+                HostName = _mQConfiguration.HostName,
+                Port = _mQConfiguration.Port,
+                UserName = _mQConfiguration.UserName,
+                Password = _mQConfiguration.Password
+            };
+            // 2.建立连接
+            using var conn = factory.CreateConnection();
+            // 3.创建频道 
+            using var channel = conn.CreateModel();
+            // 4.定义交换机
+            channel.ExchangeDeclare(exchangeName, exchangeType, true, false, null);
+            // 声明队列
+            queueName = channel.QueueDeclare().QueueName;
+            //绑定交换机-队列
+            channel.QueueBind(queueName, exchangeName, routingKey, null);
+            //设置prefetchCount : 1来告知RabbitMQ，在未收到消费端的消息确认时，不再分发消息，也就确保了当消费端处于忙碌状态时，不再分配任务。
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+            // 5.构造消费者实例
+            var consumer = new EventingBasicConsumer(channel);
+            // 6.绑定消息接收后的事件委托
+            consumer.Received += (model, ea) =>
+            {
+                var msg = "";
+                var bytes = ea.Body;
+                msg = Encoding.UTF8.GetString(bytes);
+                var instance = JsonConvert.DeserializeObject<T>(msg);
+                // 7. 模拟耗时
+                var success = func(instance);
+                //应答确认
+                if (success)
                 {
-                    // 实例化连接工厂。
-                    var factory = new ConnectionFactory
-                    {
-                        VirtualHost = "/",
-                        HostName = "localhost",
-                        Port = 5672,
-                        UserName = _mQConfiguration.UserName ?? "",
-                        Password = _mQConfiguration.Password ?? ""
-                    };
-                    // 创建连接、信道。
-                    using var conn = factory.CreateConnection();
-                    using var channel = conn.CreateModel();
-                    //定义交换机
-                    channel.ExchangeDeclare(exchangeName, exchangeType, true, false, null);
-                    // 获取发送消息
-                    channel.QueueDeclare(queueName, true, false, false, null);
-                    //绑定交换机-队列
-                    channel.QueueBind(queueName, exchangeName, routingKey, null);
-                    // 告知 RabbitMQ，在未收到当前 Worker 的消息确认信号时，不再分发给消息，确保公平调度。
-                    channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
-                    //设置消息持久化
-                    var props = channel.CreateBasicProperties();
-                    // MIME类型
-                    props.ContentType = "text/plain";
-                    //非持久化1,持久化2
-                    props.DeliveryMode = 2;
-                    //在队列上定义一个消费者
-                    var consumer = new EventingBasicConsumer(channel);
-
-                    var closeErrorCount = 0;//队列服务关闭重试次数
-
-                    // 绑定消息接收事件。
-                    consumer.Received += (model, ea) =>
-                    {
-                        var msg = "";
-                        try
-                        {
-                            if (channel.IsClosed)
-                            {
-                                isClose = true;
-                                return;
-                            }
-
-                            var bytes = ea.Body;
-                            msg = Encoding.UTF8.GetString(bytes);
-                            var instance = JsonConvert.DeserializeObject<T>(msg);
-                            var success = func(instance);
-                            //应答确认
-                            if (success)
-                            {
-                                // 手动发送消息确认信号。
-                                channel.BasicAck(ea.DeliveryTag, false);
-                                if (closeErrorCount > 0)
-                                    closeErrorCount = 0;
-                            }
-                            else
-                            {
-                                //消息未确认
-                            }
-                        }
-                        catch (EndOfStreamException)//捕获队列服务端关闭的异常
-                        {
-                            isClose = true;
-                            return;
-                        }
-                        catch (Exception ex)
-                        {
-                            isClose = true;
-                            return;
-                        }
-                    };
-                    // 消费队列，并设置应答模式为程序主动应答
-                    // autoAck:false - 关闭自动消息确认，调用`BasicAck`方法进行手动消息确认。
-                    // autoAck:true  - 开启自动消息确认，当消费者接收到消息后就自动发送 ack 信号，无论消息是否正确处理完毕。
-                    channel.BasicConsume(queueName, false, consumer);
+                    // 8.发送消息确认信号（手动消息确认）
+                    channel.BasicAck(ea.DeliveryTag, false);
                 }
-                catch (Exception)
-                {
-                    isClose = true;
-                }
-                System.Threading.Thread.Sleep(2 * 60 * 1000);//休息 5 分钟重新连接队列服务端；
-            } while (isClose);
+                // 9.启动消费者
+            };
+            //autoAck:true；自动进行消息确认，当消费端接收到消息后，就自动发送ack信号，不管消息是否正确处理完毕
+            //autoAck:false；关闭自动消息确认，通过调用BasicAck方法手动进行消息确认
+            channel.BasicConsume(queueName, false, consumer);
+        }
+        /// <summary>
+        /// 异步消息订阅-Exchange模式
+        /// </summary>
+        /// <typeparam name="T">消息模型</typeparam>
+        /// <param name="queueName">队列名称（空则为临时队列）</param>
+        /// <param name="exchangeName">交换机名称</param>
+        /// <param name="exchangeType">交换机类型</param>
+        /// <param name="routingKey">路由键</param>
+        /// <param name="func">处理消息</param>
+        public void SubscribeAsync<T>(string queueName, string exchangeName, string exchangeType, string routingKey, Func<T, Task<bool>> func) where T : class
+        {
+            // 1.实例化连接工厂。
+            var factory = new ConnectionFactory
+            {
+                VirtualHost = "/",
+                HostName = _mQConfiguration.HostName,
+                Port = _mQConfiguration.Port,
+                UserName = _mQConfiguration.UserName,
+                Password = _mQConfiguration.Password
+            };
+            // 2.建立连接
+            using var conn = factory.CreateConnection();
+            // 3.创建频道 
+            using var channel = conn.CreateModel();
+            // 4.定义交换机
+            channel.ExchangeDeclare(exchangeName, exchangeType, true, false, null);
+            // 声明队列
+            queueName = channel.QueueDeclare().QueueName;
+            //绑定交换机-队列
+            channel.QueueBind(queueName, exchangeName, routingKey, null);
+            //设置prefetchCount : 1来告知RabbitMQ，在未收到消费端的消息确认时，不再分发消息，也就确保了当消费端处于忙碌状态时，不再分配任务。
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+            // 5.构造消费者实例
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            // 6.绑定消息接收后的事件委托
+            consumer.Received += async (model, ea) =>
+             {
+                 var msg = "";
+                 var bytes = ea.Body;
+                 msg = Encoding.UTF8.GetString(bytes);
+                 var instance = JsonConvert.DeserializeObject<T>(msg);
+                 // 7. 模拟耗时
+                 var success = await func(instance);
+                 //应答确认
+                 if (success)
+                 {
+                     // 8.发送消息确认信号（手动消息确认）
+                     channel.BasicAck(ea.DeliveryTag, false);
+                 }
+             };
+            // 9.启动消费者
+            //autoAck:true；自动进行消息确认，当消费端接收到消息后，就自动发送ack信号，不管消息是否正确处理完毕
+            //autoAck:false；关闭自动消息确认，通过调用BasicAck方法手动进行消息确认
+            channel.BasicConsume(queueName, false, consumer);
         }
     }
 }
--- test/Virgo.RabbitMQ.Tests/RabbitMQProxy_Tests.cs
+++ test/Virgo.RabbitMQ.Tests/RabbitMQProxy_Tests.cs
@@ -1,8 +1,11 @@
 ﻿using RabbitMQ.Client;
+using Shouldly;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Virgo.RabbitMQ.Tests
 {
@@ -12,31 +15,35 @@ namespace Virgo.RabbitMQ.Tests
     public class RabbitMQProxy_Tests
     {
         private readonly RabbitMQProxy _rabbitMQProxy;
-        public RabbitMQProxy_Tests()
+        protected readonly ITestOutputHelper Output;
+        public RabbitMQProxy_Tests(ITestOutputHelper testOutputHelper)
         {
+            Output = testOutputHelper;
             _rabbitMQProxy = new RabbitMQProxy(new RabbitMQConfiguration
             {
                 HostName = "localhost",
                 Port = 5672,
-                UserName = "guest",
-                Password = "guest"
+                UserName = "root",
+                Password = "123456"
             });
         }
 
         [Fact]
-        public void Publish_Test()
+        public void Publish_Worker_Test()
         {
             var message = new QueueMessage()
             {
                 Name = "Jon"
             };
-            _rabbitMQProxy.Publish("topic.virgo.user", message, "exchangeJon", ExchangeType.Topic, "routingKeyByJon");
+            _rabbitMQProxy.Publish("work_queue", message);
         }
         [Fact]
-        public void Subscribe_Test()
+        public void Subscribe_Worker_Test()
         {
-            _rabbitMQProxy.Subscribe<QueueMessage>("topic.virgo.user", "exchangeJon", ExchangeType.Topic, "routingKeyByJon", (item) =>
+            _rabbitMQProxy.Subscribe<QueueMessage>("work_queue", (item) =>
              {
+                 Output.WriteLine($"来自work_queue队列消息：{item.Name}");
+                 Output.WriteLine($"来自work_queue队列消息：{item.SendTime}");
                  return true;
              });
         }
