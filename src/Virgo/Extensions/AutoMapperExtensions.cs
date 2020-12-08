5e9a6f72a39ec5bc99f38fb1690665052ca8c843
明熙 <893703953@qq.com>
2020-12-08 13:58:18 +0800

    AutoMapperExtensions

--- src/Virgo/Extensions/AutoMapperExtensions.cs
+++ src/Virgo/Extensions/AutoMapperExtensions.cs
@@ -10,36 +10,112 @@ using System.Text;
 namespace Virgo.Extensions
 {
     /// <summary>
-    /// AutoMapper映射拓展
+    /// AutoMapper扩展帮助类
     /// </summary>
     public static class AutoMapperExtensions
     {
         /// <summary>
-        /// 将源类型对象转换为目标类型对象
+        /// 类型映射
         /// </summary>
-        /// <typeparam name="TSource">源类型</typeparam>
-        /// <typeparam name="TDestination">目标类型</typeparam>
-        /// <param name="source">原类型对象</param>
-        /// <returns>目标类型对象</returns>
-        public static TDestination Map<TSource, TDestination>(this TSource source)
+        /// <typeparam name="TDestination">映射后的对象</typeparam>
+        /// <param name="obj">要映射的对象</param>
+        /// <returns></returns>
+        public static TDestination MapTo<TDestination>(this object obj) where TDestination : class
         {
-            var config = new MapperConfiguration(cfg => cfg.CreateMap<TSource, TDestination>());
+            if (obj == null) return default(TDestination);
+
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<TDestination, object>());
             var mapper = config.CreateMapper();
-            return mapper.Map<TSource, TDestination>(source);
+            return mapper.Map<TDestination>(obj);
         }
 
         /// <summary>
-        /// 将源类型对象转换为目标类型对象集合
+        /// 集合列表类型映射
         /// </summary>
-        /// <typeparam name="TSource">源类型</typeparam>
-        /// <typeparam name="TDestination">目标类型</typeparam>
-        /// <param name="source">源类型对象集合</param>
-        /// <returns>目标类型对象集合</returns>
-        public static List<TDestination> MapToList<TSource, TDestination>(this IEnumerable<TSource> source)
+        /// <typeparam name="TDestination">目标对象类型</typeparam>
+        /// <param name="source">数据源</param>
+        /// <returns></returns>
+        public static List<TDestination> MapTo<TDestination>(this IEnumerable source) where TDestination : class
         {
-            var config = new MapperConfiguration(cfg => cfg.CreateMap<List<TSource>, List<TDestination>>());
+            if (source == null) return default(List<TDestination>);
+
+            var config = new MapperConfiguration(cfg => cfg.CreateMap(source.GetType(), typeof(TDestination)));
             var mapper = config.CreateMapper();
             return mapper.Map<List<TDestination>>(source);
+        }
+
+        /// <summary>
+        /// 集合列表类型映射
+        /// </summary>
+        /// <typeparam name="TSource">数据源类型</typeparam>
+        /// <typeparam name="TDestination">目标对象类型</typeparam>
+        /// <param name="source">数据源</param>
+        /// <returns></returns>
+        public static List<TDestination> MapTo<TSource, TDestination>(this IEnumerable<TSource> source)
+            where TDestination : class
+            where TSource : class
+        {
+            if (source == null) return new List<TDestination>();
+
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<TSource, TDestination>());
+            var mapper = config.CreateMapper();
+            return mapper.Map<List<TDestination>>(source);
+        }
+
+        /// <summary>
+        /// 集合列表类型映射
+        /// </summary>
+        /// <typeparam name="TSource">数据源类型</typeparam>
+        /// <typeparam name="TDestination">目标对象类型</typeparam>
+        /// <param name="source">数据源</param>
+        /// <param name="configure">自定义配置</param>
+        /// <returns></returns>
+        public static List<TDestination> MapTo<TSource, TDestination>(this IEnumerable<TSource> source, Action<IMapperConfigurationExpression> configure)
+            where TDestination : class
+            where TSource : class
+        {
+            if (source == null) return new List<TDestination>();
+
+            var config = new MapperConfiguration(configure);
+            var mapper = config.CreateMapper();
+            return mapper.Map<List<TDestination>>(source);
+        }
+
+        /// <summary>
+        /// 类型映射
+        /// </summary>
+        /// <typeparam name="TSource">数据源类型</typeparam>
+        /// <typeparam name="TDestination">目标对象类型</typeparam>
+        /// <param name="source">数据源</param>
+        /// <param name="destination">目标对象</param>
+        /// <returns></returns>
+        public static TDestination MapTo<TSource, TDestination>(this TSource source, TDestination destination)
+            where TSource : class
+            where TDestination : class
+        {
+            if (source == null) return destination;
+
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<TSource, TDestination>());
+            var mapper = config.CreateMapper();
+            return mapper.Map<TSource, TDestination>(source, destination);
+        }
+
+        /// <summary>
+        /// 类型映射,默认字段名字一一对应
+        /// </summary>
+        /// <typeparam name="TDestination">转化之后的model，可以理解为viewmodel</typeparam>
+        /// <typeparam name="TSource">要被转化的实体，Entity</typeparam>
+        /// <param name="source">可以使用这个扩展方法的类型，任何引用类型</param>
+        /// <returns>转化之后的实体</returns>
+        public static TDestination MapTo<TSource, TDestination>(this TSource source)
+            where TDestination : class
+            where TSource : class
+        {
+            if (source == null) return default(TDestination);
+
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<TSource, TDestination>());
+            var mapper = config.CreateMapper();
+            return mapper.Map<TDestination>(source);
         }
     }
 }
