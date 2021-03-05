26a31f59bef23922c56f93f78b02aed8f6253378
刘伟超 <liuweichao@inexten.com>
2021-03-05 17:13:04 +0800

    增加IP拓展

--- src/Virgo/Extensions/WebExtension.cs
+++ src/Virgo/Extensions/WebExtension.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Virgo.Extensions
@@ -24,6 +30,78 @@ namespace Virgo.Extensions
                 .Append(request.Path)
                 .Append(request.QueryString)
                 .ToString();
+        }
+
+        public static string GetRequestIP(this IHttpContextAccessor httpContextAccessor, bool tryUseXForwardHeader = true)
+        {
+            string ip = null;
+
+            // todo support new "Forwarded" header (2014) https://en.wikipedia.org/wiki/X-Forwarded-For
+
+            // X-Forwarded-For (csv list):  Using the First entry in the list seems to work
+            // for 99% of cases however it has been suggested that a better (although tedious)
+            // approach might be to read each IP from right to left and use the first public IP.
+            // http://stackoverflow.com/a/43554000/538763
+            //
+            if (tryUseXForwardHeader)
+                ip = httpContextAccessor.GetHeaderValueAs<string>("X-Forwarded-For").SplitCsv().FirstOrDefault();
+
+            // RemoteIpAddress is always null in DNX RC1 Update1 (bug).
+            if (ip.IsNullOrWhitespace() && httpContextAccessor.HttpContext?.Connection?.RemoteIpAddress != null)
+                ip = httpContextAccessor.HttpContext.Connection.RemoteIpAddress.ToString();
+
+            if (ip.IsNullOrWhitespace())
+                ip = httpContextAccessor.GetHeaderValueAs<string>("REMOTE_ADDR");
+
+            // _httpContextAccessor.HttpContext?.Request?.Host this is the local host.
+
+            if (ip.IsNullOrWhitespace())
+                throw new Exception("Unable to determine caller's IP.");
+
+            return ip;
+        }
+
+        public static T GetHeaderValueAs<T>(this IHttpContextAccessor httpContextAccessor, string headerName)
+        {
+            StringValues values;
+
+            if (httpContextAccessor.HttpContext?.Request?.Headers?.TryGetValue(headerName, out values) ?? false)
+            {
+                string rawValues = values.ToString();   // writes out as Csv when there are multiple.
+
+                if (!rawValues.IsNullOrWhitespace())
+                    return (T)Convert.ChangeType(values.ToString(), typeof(T));
+            }
+            return default(T);
+        }
+
+        public static List<string> SplitCsv(this string csvList, bool nullOrWhitespaceInputReturnsNull = false)
+        {
+            if (string.IsNullOrWhiteSpace(csvList))
+                return nullOrWhitespaceInputReturnsNull ? null : new List<string>();
+
+            return csvList
+                .TrimEnd(',')
+                .Split(',')
+                .AsEnumerable<string>()
+                .Select(s => s.Trim())
+                .ToList();
+        }
+        public static bool IsNullOrWhitespace(this string s)
+        {
+            return String.IsNullOrWhiteSpace(s);
+        }
+    }
+
+    public static class ForwardedHeadersExtensions
+    {
+        public static IApplicationBuilder UseForwardedHeaders(this IApplicationBuilder app)
+        {
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+            return app;
         }
     }
 }
