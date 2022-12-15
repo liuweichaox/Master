ba368026e415cacf146a7201de4cc63641c2b2a5
刘大大 <33384844+lwc1st@users.noreply.github.com>
2022-12-15 09:21:52 +0800

    Create CustomerRegisteredEventHandler.cs

---
+++ API/Velen.Application/Customers/IntegrationHandlers/CustomerRegisteredEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System.Text.Json;
+using Velen.Domain.Customers;
+
+namespace Velen.Application.Customers.IntegrationHandlers
+{
+    public class CustomerRegisteredEventHandler : INotificationHandler<CustomerRegisteredEvent>
+    {
+        public Task Handle(CustomerRegisteredEvent notification, CancellationToken cancellationToken)
+        {
+            Console.WriteLine("CustomerRegisteredEventHandler - Handle command json " + JsonSerializer.Serialize(notification));
+            return Task.CompletedTask;
+        }
+    }
+}
