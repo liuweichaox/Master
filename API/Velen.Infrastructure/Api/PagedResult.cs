b84cdb047ed9dc4cfe54cb6f8bd1d7c1c2b999d7
刘大大 <893703953@qq.com>
2023-05-19 23:08:31 +0800

    Update PagedResult.cs

--- API/Velen.Infrastructure/Api/PagedResult.cs
+++ API/Velen.Infrastructure/Api/PagedResult.cs
@@ -2,108 +2,50 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Velen.Infrastructure.Api;
 
-public class PagedResult<T>
+/// <summary>
+/// 分页结果
+/// </summary>
+/// <typeparam name="TResult"></typeparam>
+public class PagedResult<TResult>
 {
-    public PagedResult(int totalItems,
-        int pageNumber = 1,
-        int pageSize = 10,
-        int maxNavigationPages = 5)
+    public PagedResult() { }
+
+    public PagedResult(IEnumerable<TResult> items, int totalCount, int pageIndex, int pageSize)
     {
-        // Calculate total pages
-        var totalPages = (int)Math.Ceiling(totalItems / (decimal)pageSize);
-
-        // Ensure actual page isn't out of range
-        if (pageNumber < 1)
-        {
-            pageNumber = 1;
-        }
-        else if (pageNumber > totalPages)
-        {
-            pageNumber = totalPages;
-        }
-
-        int startPage;
-        int endPage;
-        if (totalPages <= maxNavigationPages)
-        {
-            startPage = 1;
-            endPage = totalPages;
-        }
-        else
-        {
-            var maxPagesBeforeActualPage = (int)Math.Floor(maxNavigationPages / (decimal)2);
-            var maxPagesAfterActualPage = (int)Math.Ceiling(maxNavigationPages / (decimal)2) - 1;
-            if (pageNumber <= maxPagesBeforeActualPage)
-            {
-                // Page at the start
-                startPage = 1;
-                endPage = maxNavigationPages;
-            }
-            else if (pageNumber + maxPagesAfterActualPage >= totalPages)
-            {
-                // Page at the end
-                startPage = totalPages - maxNavigationPages + 1;
-                endPage = totalPages;
-            }
-            else
-            {
-                // Page in the middle
-                startPage = pageNumber - maxPagesBeforeActualPage;
-                endPage = pageNumber + maxPagesAfterActualPage;
-            }
-        }
-
-        // Create list of Page numbers
-        var pageNumbers = Enumerable.Range(startPage, (endPage + 1) - startPage);
-
-        StartPage = startPage;
-        EndPage = endPage;
-        PageNumber = pageNumber;
-        PageNumbers = pageNumbers;
+        Results = items;
+        TotalCount = totalCount;
+        PageCount = (totalCount + pageSize - 1) / pageSize;
+        PageIndex = pageIndex;
         PageSize = pageSize;
-        TotalItems = totalItems;
-        TotalPages = totalPages;
+        HasPrev = pageIndex > 1;
+        HasNext = pageIndex < PageCount;
     }
-
-    public IEnumerable<T> Items { get; set; }
-
     /// <summary>
-    /// Total number of items to be paged
+    /// 分页集合
     /// </summary>
-    public int TotalItems { get; set; }
-
+    public virtual IEnumerable<TResult> Results { get; private set; }
     /// <summary>
-    /// Maximum number of page navigation links to display, default is 5
+    /// 总数据量
     /// </summary>
-    public int MaxNavigationPages { get; private set; } = 5;
-
+    public virtual int TotalCount { get; private set; }
     /// <summary>
-    /// Current active page
+    /// 总页码
     /// </summary>
-    public int PageNumber { get; private set; } = 1;
-
+    public virtual int PageCount { get; private set; }
     /// <summary>
-    /// Number of items per page, default is 10
+    /// 当前页码
     /// </summary>
-    public int PageSize { get; private set; } = 10;
-
+    public virtual int PageIndex { get; private set; }
     /// <summary>
-    /// Total number of pages
+    /// 页码大小
     /// </summary>
-    public int TotalPages { get; private set; }
-
+    public virtual int PageSize { get; private set; }
     /// <summary>
-    /// Start Page number
-    /// </summary>        
-    public int StartPage { get; private set; }
-
-    /// <summary>
-    /// End Page number
-    /// </summary>        
-    public int EndPage { get; private set; }
-
-    /// <summary>
-    /// List of page numbers that we can loop
+    /// 是否有上一页
     /// </summary>
-    public IEnumerable<int> PageNumbers { get; private set; }
+    public virtual bool HasPrev { get; private set; }
+    /// <summary>
+    /// 是否有下一页
+    /// </summary>
+    public virtual bool HasNext { get; private set; }
 }
