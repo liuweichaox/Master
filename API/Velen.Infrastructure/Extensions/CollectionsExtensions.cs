ce979225523fbbe667b73324eb05d835b7f1dcfe
Waychao Liu <liuweichaox@gmail.com>
2023-08-21 00:22:53 +0800

    Update CollectionsExtensions.cs

--- API/Velen.Infrastructure/Extensions/CollectionsExtensions.cs
+++ API/Velen.Infrastructure/Extensions/CollectionsExtensions.cs
@@ -5,74 +5,6 @@ namespace Velen.Infrastructure.Extensions;
 /// </summary>
 public static class CollectionsExtensions
 {
-    #region SyncForEach
-
-    /// <summary>
-    /// 遍历数组
-    /// </summary>
-    /// <param name="objs"></param>
-    /// <param name="action">回调方法</param>
-    public static void ForEach(this object[] objs, Action<object> action)
-    {
-        foreach (var o in objs)
-        {
-            action(o);
-        }
-    }
-
-    /// <summary>
-    /// 遍历 IEnumerable
-    /// </summary>
-    /// <param name="objs"></param>
-    /// <param name="action">回调方法</param>
-    public static void ForEach(this IEnumerable<dynamic> objs, Action<object> action)
-    {
-        foreach (var o in  objs)
-        {
-            action(o);
-        }
-    }
-    
-    /// <summary>
-    /// 遍历 IAsyncEnumerable
-    /// </summary>
-    /// <param name="objs"></param>
-    /// <param name="action">回调方法</param>
-    public static async Task ForEach(this IAsyncEnumerable<dynamic> objs, Action<object> action)
-    {
-        await foreach (var o in  objs)
-        {
-            action(o);
-        }
-    }
-
-    /// <summary>
-    /// 遍历集合
-    /// </summary>
-    /// <param name="objs"></param>
-    /// <param name="action">回调方法</param>
-    public static void ForEach(this IList<dynamic> objs, Action<object> action)
-    {
-        foreach (var o in objs)
-        {
-            action(o);
-        }
-    }
-
-    /// <summary>
-    /// 遍历数组
-    /// </summary>
-    /// <param name="objs"></param>
-    /// <param name="action">回调方法</param>
-    /// <typeparam name="T"></typeparam>
-    public static void ForEach<T>(this T[] objs, Action<T> action)
-    {
-        foreach (var o in objs)
-        {
-            action(o);
-        }
-    }
-
     /// <summary>
     /// 遍历 IEnumerable
     /// </summary>
@@ -86,49 +18,7 @@ public static class CollectionsExtensions
             action(o);
         }
     }
-
-    /// <summary>
-    /// 遍历 IAsyncEnumerable
-    /// </summary>
-    /// <param name="objs"></param>
-    /// <param name="action">回调方法</param>
-    /// <typeparam name="T"></typeparam>
-    public static async Task ForEach<T>(this IAsyncEnumerable<T> objs, Action<T> action)
-    {
-        await foreach (var o in objs)
-        {
-            action(o);
-        }
-    }
     
-    /// <summary>
-    /// 遍历 List
-    /// </summary>
-    /// <param name="objs"></param>
-    /// <param name="action">回调方法</param>
-    /// <typeparam name="T"></typeparam>
-    public static void ForEach<T>(this IList<T> objs, Action<T> action)
-    {
-        foreach (var o in objs)
-        {
-            action(o);
-        }
-    }
-
-    /// <summary>
-    /// 遍历数组并返回一个新的 List
-    /// </summary>
-    /// <param name="objs"></param>
-    /// <param name="action">回调方法</param>
-    /// <returns></returns>
-    public static IEnumerable<T> ForEach<T>(this object[] objs, Func<object, T> action)
-    {
-        foreach (var o in objs)
-        {
-            yield return action(o);
-        }
-    }
-
     /// <summary>
     /// 遍历 IEnumerable 并返回一个新的 List
     /// </summary>
@@ -143,7 +33,7 @@ public static class CollectionsExtensions
             yield return action(o);
         }
     }
-
+    
     /// <summary>
     /// 遍历 IAsyncEnumerable 并返回一个新的 List
     /// </summary>
@@ -151,7 +41,7 @@ public static class CollectionsExtensions
     /// <param name="action">回调方法</param>
     /// <typeparam name="T"></typeparam>
     /// <returns></returns>
-    public static async IAsyncEnumerable<T> ForEach<T>(this IAsyncEnumerable<dynamic> objs, Func<object, T> action)
+    public static async IAsyncEnumerable<T> ForEachAsync<T>(this IAsyncEnumerable<dynamic> objs, Func<object, T> action)
     {
         await foreach (var o in objs)
         {
@@ -159,107 +49,6 @@ public static class CollectionsExtensions
         }
     }
     
-    /// <summary>
-    /// 遍历List并返回一个新的List
-    /// </summary>
-    /// <param name="objs"></param>
-    /// <param name="action">回调方法</param>
-    /// <typeparam name="T"></typeparam>
-    /// <returns></returns>
-    public static IEnumerable<T> ForEach<T>(this IList<dynamic> objs, Func<object, T> action)
-    {
-        foreach (var o in objs)
-        {
-            yield return action(o);
-        }
-    }
-
-
-    /// <summary>
-    /// 遍历数组并返回一个新的 List
-    /// </summary>
-    /// <param name="objs"></param>
-    /// <param name="action">回调方法</param>
-    /// <typeparam name="T"></typeparam>
-    /// <returns></returns>
-    public static IEnumerable<T> ForEach<T>(this T[] objs, Func<T, T> action)
-    {
-        foreach (var o in objs)
-        {
-            yield return action(o);
-        }
-    }
-
-    /// <summary>
-    /// 遍历 IEnumerable 并返回一个新的 List
-    /// </summary>
-    /// <param name="objs"></param>
-    /// <param name="action">回调方法</param>
-    /// <typeparam name="T"></typeparam>
-    /// <returns></returns>
-    public static IEnumerable<T> ForEach<T>(this IEnumerable<T> objs, Func<T, T> action)
-    {
-        foreach (var o in objs)
-        {
-            yield return action(o);
-        }
-    }
-
-    /// <summary>
-    /// 遍历 IAsyncEnumerable 并返回一个新的 List
-    /// </summary>
-    /// <param name="objs"></param>
-    /// <param name="action">回调方法</param>
-    /// <typeparam name="T"></typeparam>
-    /// <returns></returns>
-    public static async IAsyncEnumerable<T> ForEach<T>(this IAsyncEnumerable<T> objs, Func<T, T> action)
-    {
-        await foreach (var o in objs)
-        {
-            yield return action(o);
-        }
-    }
-    
-    /// <summary>
-    /// 遍历 List 并返回一个新的 List
-    /// </summary>
-    /// <param name="objs"></param>
-    /// <param name="action">回调方法</param>
-    /// <typeparam name="T"></typeparam>
-    /// <returns></returns>
-    public static IEnumerable<T> ForEach<T>(this IList<T> objs, Func<T, T> action)
-    {
-        foreach (var o in objs)
-        {
-            yield return action(o);
-        }
-    }
-    
-    #endregion
-
-    #region AsyncForEach
-
-    /// <summary>
-    /// 遍历数组
-    /// </summary>
-    /// <param name="objs"></param>
-    /// <param name="action">回调方法</param>
-    public static async void ForEachAsync(this object[] objs, Action<object> action)
-    {
-        await Task.Run(() => { Parallel.ForEach(objs, action); });
-    }
-
-    /// <summary>
-    /// 遍历数组
-    /// </summary>
-    /// <param name="objs"></param>
-    /// <param name="action">回调方法</param>
-    /// <typeparam name="T"></typeparam>
-    public static async void ForEachAsync<T>(this T[] objs, Action<T> action)
-    {
-        await Task.Run(() => { Parallel.ForEach(objs, action); });
-    }
-
     /// <summary>
     /// 遍历 IEnumerable
     /// </summary>
@@ -271,25 +60,29 @@ public static class CollectionsExtensions
         await Task.Run(() => { Parallel.ForEach(objs, action); });
     }
 
+    
     /// <summary>
-    /// 遍历 List
+    /// 遍历 IAsyncEnumerable
     /// </summary>
     /// <param name="objs"></param>
     /// <param name="action">回调方法</param>
     /// <typeparam name="T"></typeparam>
-    public static async void ForEachAsync<T>(this IList<T> objs, Action<T> action)
+    public static async Task ForEachAsync<T>(this IAsyncEnumerable<T> objs, Action<T> action)
     {
-        await Task.Run(() => { Parallel.ForEach(objs, action); });
+        await foreach (var o in objs)
+        {
+            action(o);
+        }
     }
-
-    #endregion
-
-    #region Valid
-
+    
+    /// <summary>
+    /// 是否为空
+    /// </summary>
+    /// <param name="objs"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
     public static bool IsNullOrEmpty<T>(this IEnumerable<T> objs)
     {
         return objs == null || objs.Count() == 0;
     }
-
-    #endregion
 }
